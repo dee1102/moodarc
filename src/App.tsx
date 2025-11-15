6759cc5a18f744b7f946b4c59e8b1e7e346cb3cc
devarshi.vyas <devarshi110297@gmail.com>
2025-11-14 21:15:03 -0500

    feat: Added basic task component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,30 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import TaskComponent from "./TaskComponent";
+
+
+import { useState } from 'react';
+import dayjs from "dayjs";
 
 
 function App() {
-  return <div><CalendarComponent /></div>
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+
+  return (
+    // <div className="rowC">
+    //   <CalendarComponent selectedDateState={(newValue) => setSelectedDate(newValue)} />
+    //   <TaskComponent selectedDate={selectedDate}/>
+    // </div>
+    <div style={{ display: 'flex', flexDirection: 'row', gap: 16, alignItems: 'flex-start' }}>
+      <div style={{ width: 360 /* or desired calendar width */ }}>
+        <CalendarComponent selectedDateState={(newValue) => setSelectedDate(newValue)} />
+      </div>
+      <div style={{ flex: 1 }}>
+        <TaskComponent selectedDate={selectedDate}/>
+      </div>
+    </div>
+  )
 }
 
 export default App;
